96bfc316db0d876b226ff58beef9f8ff8f5fe6b7
Jakub Malczak <j.malczak@samsung.local>
2014-05-05 14:31:46 +0200

    Added scala week 1 assignment 1

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,36 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r){
+      return 1
+    }
+    else
+    {
+      return pascal(c -1, r -1) + pascal(c, r -1)
+    }
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = true
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = 2
+}
