7a4af3a6108eca1ec227a99cd4e74466863c2de8
Jakub Malczak <j.malczak@samsung.local>
2014-05-05 16:15:05 +0200

    Added second assignment

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,19 +15,29 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if(c == 0 || c == r){
-      return 1
-    }
-    else
-    {
-      return pascal(c -1, r -1) + pascal(c, r -1)
-    }
+    if(c == 0 || c == r) return 1   
+    else return pascal(c -1, r -1) + pascal(c, r -1)    
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = true
+  def balance(chars: List[Char]): Boolean = {
+     
+    def balance(chars: List[Char], beginNumber: Int): Boolean = {
+      
+      if(beginNumber == 0 && chars.length == 0) return true
+      if(beginNumber < 0) return false
+      if(beginNumber > 0 && chars.length == 0) return false
+      else {
+        if(chars.head == '(') return balance(chars.tail, beginNumber + 1)
+        if(chars.head == ')') return balance(chars.tail, beginNumber - 1)
+        else return balance(chars.tail, beginNumber)
+      }            
+    }
+       
+    return balance(chars, 0)
+  }
 
   /**
    * Exercise 3
