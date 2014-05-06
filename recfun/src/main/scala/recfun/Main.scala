82cfc7900d17172246a1cc7d634c490e288a206e
Jakub Malczak <jmalczak@gmail.com>
2014-05-06 21:22:43 +0200

    Added 3rd assignment

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,27 +15,27 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if(c == 0 || c == r) return 1   
-    else return pascal(c -1, r -1) + pascal(c, r -1)    
+    if (c == 0 || c == r) return 1
+    else return pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-     
+
     def balance(chars: List[Char], beginNumber: Int): Boolean = {
-      
-      if(beginNumber == 0 && chars.isEmpty) return true
-      if(beginNumber < 0) return false
-      if(beginNumber > 0 && chars.isEmpty) return false
+
+      if (beginNumber == 0 && chars.isEmpty) return true
+      if (beginNumber < 0) return false
+      if (beginNumber > 0 && chars.isEmpty) return false
       else {
-        if(chars.head == '(') return balance(chars.tail, beginNumber + 1)
-        if(chars.head == ')') return balance(chars.tail, beginNumber - 1)
+        if (chars.head == '(') return balance(chars.tail, beginNumber + 1)
+        if (chars.head == ')') return balance(chars.tail, beginNumber - 1)
         else return balance(chars.tail, beginNumber)
-      }            
+      }
     }
-       
+
     return balance(chars, 0)
   }
 
@@ -43,26 +43,13 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    
-    print(coins.toString + " money " + money +  "\n")
-    
-    if(money == 0) return 0
-    if(coins.isEmpty) return 0
-    else{
-         
-      def separateCountChanges =  for(i <- coins) yield {	
-        
-        if(money == i) 1
-        else {
-          if(money < i) 0
-          else countChange(money - i, coins)
-        }
-      }
-            
-      print(separateCountChanges.toString + "\n")
-  
-      
-      return if(separateCountChanges.forall(p => p == 1)) 1 else 0 
-    }
+
+    if (money == 0) return 0
+    if (money < 0) return 0
+    if (coins.isEmpty) return 0
+    else return (if (money == coins.head) 1 else countChange(money - coins.head, coins)) + countChange(money, coins.tail)
   }
 }
+
+
+
